81edc4ba4ebc45cfa4c5a335e6c664ad4bdf9a30
Renee Geffre <reneegeffre@RGMacBook.local>
2017-08-20 18:59:46 -0400

    macpro

--- keys.js
+++ keys.js
@@ -3,6 +3,6 @@ console.log('this is loaded');
 exports.twitterKeys = {
   consumer_key: 'jZ9pfbcnpxdao5xvs0CK1kXRE',
   consumer_secret: 'Uw0VWuRxuDfYH8T0yYI9wufYsDlE5Deb9XX16GWer1aLkzV512',
-  access_token_key: '898610387955851264-dJKBFclCvQ99NBCtcxdQmJawjWYBM47',
-  access_token_secret: '7fBWoc1ILorHHjuSv90xtgS1uYaPFcyjuKztyMesvHuXF'
+  access_token_key: '898610387955851264-JMJEurv1U33vwPj6E3AG5QTsRdGj7CY',
+  access_token_secret: 'oCNcrbqJl0dqJuuvAY3kffOy35RYf9P5G8q6X8i7lfu0h'
 }
